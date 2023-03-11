46f3f28a02974ea99670ae074f111e781cbfb100
LanLan77 <60605271+LanLan77@users.noreply.github.com>
2023-03-12 00:14:13 +1030

    q17

--- question17.sh
+++ question17.sh
@@ -1,23 +1,20 @@
-git branch branch1
+git fetch origin main:branch1
+git fetch origin main:branch2
+git checkout main
+cp dir3/bar dir3/bar_copy
+git add .
+git commit -m "main"
 git checkout branch1
-mkdir dir1
-mv dir2/foo dir1/
-rm -r dir2
-mv dir3/bar .
+cp dir1/dir2/foo dir1/foo
+rm -r dir1/dir2
 touch newfile1
 git add .
-git commit -m "3"
-git checkout main
-git branch branch2
+git commit -m "branch1"
 git checkout branch2
-mkdir -p dir1/dir3
-mv dir1/foo dir1/dir2/foo_modified
+mv dir1/dir2/foo dir1/dir2/foo_modified
+rm -r dir3
+mkdir dir1/dir3
 touch dir1/dir3/newfile2
 git add .
-git commit -m "1"
-git checkout main
-mv dir3/bar_copy dir3/bar
-touch newfile1
-rm newfile1
-git add .
-git commit -m "2"
+git commit -m "branch2"
+
